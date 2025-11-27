6fdcbdd34e61e62f4d8729bd86048225e6599354
LocalRapid <sukrish14369@gmail.com>
2025-11-27 19:21:26 +0530

    product deatails page design update

--- offer countdown.js
+++ offer countdown.js
@@ -12,7 +12,7 @@
                     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
                     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
                     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-                    countdownContainer.innerHTML = `<div class="${timerClass}">Deal Ends In: ${days}d ${hours}h ${minutes}m ${seconds}s</div>`;
+                    countdownContainer.innerHTML = `<div class="${timerClass}"><i class="fa-solid fa-bomb"></i> ${days}d ${hours}h ${minutes}m ${seconds}s</div>`;
                 } else {
                     clearInterval(countdownInterval);
                     countdownContainer.innerHTML = `<div class="${timerClass}">DEAL EXPIRED</div>`;
